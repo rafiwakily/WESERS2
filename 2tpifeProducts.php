799352f928e4ac24ffd0cf2964283081e2649552
rafiwakily <rafi.wakily72@gmail.com>
2020-05-16 18:01:54 +0100

    minor changes

--- 2tpifeProducts.php
+++ 2tpifeProducts.php
@@ -15,18 +15,6 @@ include_once "displayUser.php";
 </head>
 
 <body>
-<script>
-        window.onscroll = function() {myFunction()};
-        var header = document.getElementById("myHeader");
-        var sticky = header.offsetTop;
-        function myFunction() {
-          if (window.pageYOffset > sticky) {
-            header.classList.add("sticky");
-          } else {
-            header.classList.remove("sticky");
-          }
-        }
-</script>
     <nav id="navigationBar">
         <div id="navigationTittle">
             <h1></h1>
